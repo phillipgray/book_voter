f1fe85488657a9c4fb397b7ec124c4f5916dc6e7
Phillip Gray <escapethegray11235@gmail.com>
2016-08-07 21:31:21 -0400

    Installs and configures devise for users

--- db/schema.rb
+++ db/schema.rb
@@ -10,19 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160726005338) do
+ActiveRecord::Schema.define(version: 20160808011658) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "users", force: :cascade do |t|
-    t.string   "email"
+    t.string   "email",              default: "", null: false
     t.string   "first_name"
     t.string   "last_name"
-    t.string   "password"
-    t.string   "password_confirmation"
-    t.datetime "created_at",            null: false
-    t.datetime "updated_at",            null: false
+    t.datetime "created_at",                      null: false
+    t.datetime "updated_at",                      null: false
+    t.string   "encrypted_password", default: "", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
   end
 
 end
